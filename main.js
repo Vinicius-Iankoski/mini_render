01f887957ccc5593c5bf31972665a884c5a3ee83
Vinicius-Iankoski <vine.iankoski@gmail.com>
2026-08-18 21:29:33 -0300

    pequenos testes

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import './style.css';
 import * as THREE from 'three';
 
 // youll always need 3 objects to render a scene: a scene, 
@@ -6,12 +5,25 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ 
-    canvas: document.getElementById('bg') 
-});
+const renderer = new THREE.WebGLRenderer();
 
-renderer.setPixelRatio(window.devicePixelRatio);
+// renderer
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
 
-renderer.render(scene, camera);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate( time ) {
+
+  cube.rotation.x = time / 2000;
+  cube.rotation.y = time / 1000;
+
+  renderer.render( scene, camera );
+
+}
